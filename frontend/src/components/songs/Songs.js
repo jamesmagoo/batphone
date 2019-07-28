2d1b774b92732926199e693f96693cd7abb8fc1b
jmagoo <jamesmcgauran11@gmail.com>
2019-07-28 22:28:29 +0100

    MusixMatch API Connected

---
+++ frontend/src/components/songs/Songs.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import Spinner from '../layout/Spinner';
+import Track from '../songs/Track';
+
+class Songs extends Component {
+    render() {
+        return (
+            <Consumer>
+                
+                { value => {
+                    const { track_list, heading} = value;
+
+                    if(track_list === undefined || track_list.length === 0 ){
+                        return <Spinner/>;
+                    }else{
+                        return(
+                            <React.Fragment>
+                                <h6 className="text-center mb-4">{heading}</h6>
+                                <div className="row">
+                                    {track_list.map(item =>(
+                                        <Track key = {item.track.track_id} track = {item.track}/>
+                                    )
+                                        )}
+                                </div>
+                            </React.Fragment>
+                        );
+                    }
+                }}
+            </Consumer>
+        )
+    }
+}
+
+export default Songs ;
